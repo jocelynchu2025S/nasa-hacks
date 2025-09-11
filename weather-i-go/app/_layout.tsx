32839ce295a1ffa37d0f799dcf11e6960baec902
Liz <elizabethho0511@gmail.com>
2025-09-10 21:08:24 -0400

    Create tab structure for all pages

--- weather-i-go/app/_layout.tsx
+++ weather-i-go/app/_layout.tsx
@@ -1,5 +1,23 @@
-import { Stack } from "expo-router";
+import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
 
-export default function RootLayout() {
-  return <Stack />;
+{/* intilaizes a new tab --> example name ="index", index is the tab name, after intlaize create a new file like index.tsx  */}
+
+export default function TabLayout() {
+  return (
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <Label>Home</Label>
+        <Icon sf="house.fill" drawable="custom_android_drawable" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="settings">
+        <Icon sf="gear" drawable="custom_settings_drawable" />
+        <Label>Settings</Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="test1">
+        <Icon sf="gear" drawable="custom_settings_drawable" />
+        <Label>Profile</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
+    
+  );
 }
